019366d50680c44620a530536dd7183b2f37be71
Madina <madinabaxtiyarova22@gmail.com>
2026-05-14 10:31:43 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primery: '#FF8A00',
+        secondary: '#3FA72F',
+        thirdstage: '#311F09',
+        cardBg: '#f8f7f5'
+      }
+    },
+    container: {
+      center: true,
+      padding: '15px',
+      screens: {
+         sm: '1230px'
+      }
+    }
+  },
+  plugins: [],
+}
+
